11f05c6357e8f26da7dc92ccea95865680feb8c2
junghyeonsu <jung660317@naver.com>
2022-11-25 18:28:23 +0900

    chore: test

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,21 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
+  app.on("pull_request.opened", async (context) => {
+    const pullRequestComment = context.pullRequest({
+      body: "Hello World!",
     });
-    await context.octokit.issues.createComment(issueComment);
+
+    await context.octokit.pulls.createReviewComment(pullRequestComment);
   });
+
+  // app.on("issues.opened", async (context) => {
+  //   const issueComment = context.issue({
+  //     body: "Thanks for opening this issue!",
+  //   });
+  //   await context.octokit.issues.createComment(issueComment);
+  // });
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
