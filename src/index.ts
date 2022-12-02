4864641b8f5a5dc4fc3410c5d17dc909f5dfc8df
junghyeonsu <jung660317@naver.com>
2022-12-03 01:05:57 +0900

    chore: change

--- src/index.ts
+++ src/index.ts
@@ -7,14 +7,44 @@ export = (app: Probot) => {
     });
 
     await context.octokit.pulls.createReviewComment(pullRequestComment);
+
+    // octokit.rest.repos.compareCommitsWithBasehead({
+    //   owner,
+    //   repo,
+    //   basehead,
+    // });
+
+    const { data: diff } = await context.octokit.rest.pulls.get({
+      owner: "octokit",
+      repo: "rest.js",
+      pull_number: 123,
+      mediaType: {
+        format: "diff",
+      },
+    });
+
+    console.log("diff", diff);
   });
 
-  // app.on("issues.opened", async (context) => {
-  //   const issueComment = context.issue({
-  //     body: "Thanks for opening this issue!",
-  //   });
-  //   await context.octokit.issues.createComment(issueComment);
-  // });
+
+  app.on("issues.opened", async (context) => {
+    // const issueComment = context.issue({
+    //   body: "Thanks for opening this issue!",
+    // });
+    // await context.octokit.issues.createComment(issueComment);
+
+    const { data: diff } = await context.octokit.rest.pulls.get({
+      owner: "octokit",
+      repo: "rest.js",
+      pull_number: 123,
+      mediaType: {
+        format: "diff",
+      },
+    });
+
+    console.log("diff", diff);
+    
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
